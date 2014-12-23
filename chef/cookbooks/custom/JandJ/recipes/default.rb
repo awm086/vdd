37af7c6817d019d1342e7d69c76322dda63922fd
Ali <aalmusta@its.jnj.com>
2014-12-23 16:24:57 -0500

    adding a git repo vaiable to config.json break the JandJ recipes based on component logic

--- chef/cookbooks/custom/JandJ/recipes/default.rb
+++
@@ -1,7 +0,0 @@
-git "/var/www/JandJ" do
-  repository "git@github.com:awm086/JandJ.git"
-  revision "master"
-  action :sync
-  user 'vagrant'
-  group 'vagrant'
-end
---
+++ chef/cookbooks/custom/JandJ/recipes/git_repo.rb
@@ -0,0 +1,18 @@
+# Recipe to checkout the git repo into a specified directory.
+if node["vdd"]["sites"]
+  node["vdd"]["sites"].each do |index, site|
+    repo_url= site['repo_url']
+    name = defined?(site["vhost"]["app_root"]) ? site["vhost"]["app_root"] : index
+  	name = "var/www/" + name
+    
+    git name do
+	    repository repo_url 
+	    revision "master"
+	    action :sync
+	    user 'vagrant'
+	    group 'vagrant'
+	  end
+    
+	end
+end
+
