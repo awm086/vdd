6aabd94e75fc0ad9dcead385a51c72dd523ab2e7
Ali <aalmusta@its.jnj.com>
2015-01-08 11:37:13 -0500

    solr fixes

--- chef/cookbooks/custom/drupal-chef-solr/attributes/default.rb
+++ chef/cookbooks/custom/drupal-chef-solr/attributes/default.rb
@@ -7,7 +7,7 @@
 
 default['solr']['version']  = '3.5.0'
 default['solr']['url']      = "https://archive.apache.org/dist/lucene/solr/#{node['solr']['version']}/apache-solr-#{node['solr']['version']}.tgz"
-default['solr']['data_dir'] = '/etc/solr'
+default['solr']['data_dir'] = '/opt/solr/example/multicore'
 default['solr']['dir'] = '/opt/solr'
 default['solr']['port'] = '8983'
 default['solr']['user'] = 'solr'
